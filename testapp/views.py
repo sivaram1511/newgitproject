b5a177c46a1550e4c56f8a90c248fa636f7bb3c7
sivaram <sivaramk1411@gmail.com>
2021-05-19 10:42:45 +0530

    home page created

--- testapp/views.py
+++ testapp/views.py
@@ -1,3 +1,7 @@
 from django.shortcuts import render
 
 # Create your views here.
+def home_page_view(request):
+    return render(request,'testapp/home.html')
+def java_exam_view(request):
+    return render(request,"testapp/javaexams.html")
