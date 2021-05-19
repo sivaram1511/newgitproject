5b103fe8567ca198b1d7837e4777582c5ed074ab
sivaram <sivaramk1411@gmail.com>
2021-05-19 12:32:19 +0530

    login url

--- newgitproject/urls.py
+++ newgitproject/urls.py
@@ -14,11 +14,13 @@ Including another URLconf
     2. Add a URL to urlpatterns:  path('blog/', include('blog.urls'))
 """
 from django.contrib import admin
-from django.urls import path
+from django.urls import path,include
 from testapp import views
 urlpatterns = [
     path('admin/', admin.site.urls),
     path('',views.home_page_view),
     path('java/',views.java_exam_view),
-    path('python/',views.python_exams_views),
+    path('python/',views.python_exam_view),
+    path('aptitude/',views.aptitude_exam_view),
+    path('accounts/',include('django.contrib.auth.urls')),
 ]
--- testapp/views.py
+++ testapp/views.py
@@ -1,9 +1,12 @@
 from django.shortcuts import render
-
+from django.contrib.auth.decorators import login_required
 # Create your views here.
 def home_page_view(request):
     return render(request,'testapp/home.html')
+@login_required
 def java_exam_view(request):
     return render(request,"testapp/javaexams.html")
 def python_exam_view(request):
     return render(request,"testapp/pythonexams.html")
+def aptitude_exam_view(request):
+    return render(request,"testapp/aptitudeexams.html")
